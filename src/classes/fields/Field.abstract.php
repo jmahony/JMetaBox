c72307abddf43388d4999ee54e2f96c247ef60d1
jmahony <jalmahony@gmail.com>
2013-04-17 17:08:28 +0100

    Make sure _get post is set

--- src/classes/fields/Field.abstract.php
+++ src/classes/fields/Field.abstract.php
@@ -86,8 +86,9 @@ abstract class Field extends Renderer implements FieldInterface {
     $this->label = (isset($args['label'])) ? $args['label'] : null;
 
     // Fetch the post meta, we need this to display in the field when rendering
-    $this->value = get_post_meta($_GET['post'], $this->id, true);
-
+    if (isset($_GET['post'])) {
+      $this->value = get_post_meta($_GET['post'], $this->id, true);
+    }
     // Populate directory attribute
     $this->directory = $this->getDirectory();
 
