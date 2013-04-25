f531039e0fb94073bcab73a488b53a26419a8597
jmahony <jalmahony@gmail.com>
2013-04-25 14:07:40 +0100

    Input changes

--- build/classes/Input.class.php
+++ build/classes/Input.class.php
@@ -10,12 +10,26 @@ class Input {
 
   public static function get($k, $default = null) {
 
-    return array_key_exists($k, $_GET) ? $_GET[$k] : $default;
+    return self::getHas($k) ? $_GET[$k] : $default;
 
   }
 
   public static function getHas($k) {
+
     return array_key_exists($k, $_GET);
+
+  }
+
+  public static function post($k, $default = null) {
+
+    return self::postHas($k) ? $_POST[$k] : $default;
+
+  }
+
+  public static function postHas($k) {
+
+    return array_key_exists($k, $_POST);
+
   }
 
 }
