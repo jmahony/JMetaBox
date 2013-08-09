eeb63efb86dbe3a96c9be4f7f8cd7112029172ac
Josh Mahony <jalmahony@gmail.com>
2013-08-09 17:06:07 +0100

    Add in conditions for radio buttons

--- build/classes/fields/radio/Radio.class.php
+++ build/classes/fields/radio/Radio.class.php
@@ -7,16 +7,25 @@ class Radio extends MultiField {
 
   public function render() {
 
+    $cg = (string) '';
+
+    if (isset($this->args['cond'])) {
+      $cg = new \JMetaBox\ConditionGenerator($this->args['cond'], $this);
+      $cg->render();
+    }
+
     $this->output .= sprintf(
-      '<div class="control-group">
-        <label class="control-label">%1$s</label>
+      '<div class="control-group" id="%1$s-container">
+        <label class="control-label">%2$s</label>
         <div class="controls">
-          %2$s%3$s
-        </div>
+          %3$s%4$s
+        </div>%5$s
       </div>',
+      $this->id,
       $this->label,
       $this->renderOptions(),
-      $this->renderHelpText()
+      $this->renderHelpText(),
+      $cg
     );
 
     return $this->output;
