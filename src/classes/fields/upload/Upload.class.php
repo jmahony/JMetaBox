56efc070a685beda6f39bfd68ff5e7dba775420c
Josh Mahony <jalmahony@gmail.com>
2013-08-09 16:30:26 +0100

    Added in conditions to upload field

--- src/classes/fields/upload/Upload.class.php
+++ src/classes/fields/upload/Upload.class.php
@@ -7,18 +7,26 @@ class Upload extends Field {
 
   public function render() {
 
+    $cg = (string) '';
+
+    if (isset($this->args['cond'])) {
+      $cg = new \JMetaBox\ConditionGenerator($this->args['cond'], $this);
+      $cg->render();
+    }
+
     $this->output .= sprintf(
-      '<div class="control-group">
+      '<div class="control-group" id="%1$s-container">
         <label class="control-label" for="%1$s">%2$s</label>
         <div class="controls">
           <input type="text" id="%1$s" name="%1$s" class="span6" value="%3$s" />%4$s
           <a class="jmeta-upload button insert-media add_media">Add Media</a>
-        </div>
+        </div>%5$s
       </div>',
       $this->id,
       $this->label,
       $this->value,
-      $this->renderHelpText()
+      $this->renderHelpText(),
+      $cg
     );
 
     return $this->output;
